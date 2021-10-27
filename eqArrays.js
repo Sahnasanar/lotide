d56b41a817db6e72910ca7bc679d32fa5efd25bc
sahnasanar <sahnasamad@gmail.com>
2021-10-27 09:55:20 -0400

    eq arrays added

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function (arr1,arr2) {
+  var result  = "";
+  for(let i in arr1){
+    for(let j in arr2) {
+
+    if(arr1[i] === arr2[j]){
+      result = "true"
+    }else {
+      result = "false"
+    }
+  }
+}
+    console.log(result);
+  }
+  
+  
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
