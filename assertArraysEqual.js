b8787c8a7f8115a0d2962417c703e88018b47a37
sahnasanar <sahnasamad@gmail.com>
2021-10-28 09:57:07 -0400

    removed unnecssary text from the file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -21,9 +21,3 @@ const eqArrays = function (arr1,arr2) {
       console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
     }
   };
-  eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
