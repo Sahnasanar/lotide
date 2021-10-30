f8235d1dfc1393514206c52748ab8ce880ef1b90
sahnasanar <sahnasamad@gmail.com>
2021-10-30 13:12:08 -0400

    assertObject file added

---
+++ assertObjectEqual.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function (arr1,arr2) {
+  var result  = "";
+  for(let i in arr1){
+    for(let j in arr2) {
+
+    if(arr1[i] === arr2[j]){
+      result = "true"
+    }else {
+      result = "false"
+    }
+  }
+}
+    return result;
+  }
+  const eqObjects = function (object1, object2) {
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+      return false;
+    } else {
+      for (let i in object1) {
+        if ((Array.isArray(object1[i]) && Array.isArray(object2[i]))) {
+          if (eqArrays(object1[i], object2[i]) === false) {
+            return false
+          }
+        } else {
+          if (object1[i] !== object2[i]) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+  }
+  
+  const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; 
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }  
+};
