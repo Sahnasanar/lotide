3fbae6c7980a563c77a19168436ae952e61f522c
sahnasanar <sahnasamad@gmail.com>
2021-11-01 19:58:50 -0400

    file modified with mocha chai syntax

--- middle.js
+++ middle.js
@@ -13,4 +13,4 @@
   };
 
 
-module.exports = middle.js
+module.exports = middle
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,6 @@
 // TEST CODE
 
-const assertEqual = require('./assertEqual');
+const assertEqual = require('../assertEqual');
 
 
 assertEqual("Lighthouse Labs", "Bootcamp");
