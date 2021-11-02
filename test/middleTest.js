6d9333e551f7e9b5fac999a8abdb4f4c1cacc4ee
sahnasanar <sahnasamad@gmail.com>
2021-11-02 13:22:25 -0400

    checking any files left to add

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,18 @@
 
-const assertArraysEqual= require('../assertArraysEqual.js');
+//const assertArraysEqual= require('../assertArraysEqual.js');
+//const middle = require('../middle.js');
+
+
+//assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7]))
+const assert = require('chai').assert;
 const middle = require('../middle.js');
 
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7]))
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns 'Lighthouse' for ['hello','Lighthouse','labs']", () => {
+    assert.strictEqual(middle(['hello','Lighthouse','labs']), 'Lighthouse'); 
+  });
+});
